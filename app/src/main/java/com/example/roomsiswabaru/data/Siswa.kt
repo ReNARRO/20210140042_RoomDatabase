c52af421f86649d7a6e33e24dce40947251d52a3
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-12-04 19:20:07 +0700

    Menambahkan tabel siswa

---
+++ app/src/main/java/com/example/roomsiswabaru/data/Siswa.kt
@@ -0,0 +1,12 @@
+package com.example.roomsiswabaru.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
