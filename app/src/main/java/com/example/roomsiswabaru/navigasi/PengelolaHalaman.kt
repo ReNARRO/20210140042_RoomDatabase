058156ea25763d15da16ce9deee665293b1617d9
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-12-04 23:00:40 +0700

    Membuat function Host navigasi

---
+++ app/src/main/java/com/example/roomsiswabaru/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,41 @@
+package com.example.roomsiswabaru.navigasi
+
+import android.icu.text.CaseMap.Title
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.roomsiswabaru.R
+import com.example.roomsiswabaru.ui.halaman.DestinasiEntry
+import com.example.roomsiswabaru.ui.halaman.DestinasiHome
+import com.example.roomsiswabaru.ui.halaman.EntrySiswaScreen
+import com.example.roomsiswabaru.ui.halaman.HomeScreen
+
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route)})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
