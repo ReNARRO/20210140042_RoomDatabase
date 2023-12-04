d5a2a1814972fffa2d9f0b9f54d71a156158265d
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-12-04 20:16:03 +0700

    Menambahkan data class UI State siswa

--- app/src/main/java/com/example/roomsiswabaru/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswabaru/model/EntryViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.roomsiswabaru.model
 
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
