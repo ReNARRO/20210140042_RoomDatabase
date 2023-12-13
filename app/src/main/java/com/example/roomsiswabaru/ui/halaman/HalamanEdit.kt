658c8521bf4c946841ffcc3573ab09a1319e444a
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-12-13 22:18:17 +0700

    Membuat object pada halaman edit

--- app/src/main/java/com/example/roomsiswabaru/ui/halaman/HalamanEdit.kt
+++ app/src/main/java/com/example/roomsiswabaru/ui/halaman/HalamanEdit.kt
@@ -1,2 +1,11 @@
 package com.example.roomsiswabaru.ui.halaman
 
+import com.example.roomsiswabaru.R
+import com.example.roomsiswabaru.navigasi.DestinasiNavigasi
+
+object ItemEditDestination : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
